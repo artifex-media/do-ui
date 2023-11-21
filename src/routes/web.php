55d771835eb4b0d525cf46d1c3ffce6591c5c9fe
Chloë van den Eeden <chloe@artifex-media.nl>
2023-11-21 20:22:49 +0100

    edits

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,6 +3,6 @@
 
 $namespace = "Doorons\DoUI\Http\Controllers";
 
-Route::group(['namespace' => $namespace, 'prefix' => 'dashboard','middleware' => 'auth'], function () {
+Route::group(['namespace' => $namespace, 'prefix' => 'dashboard'], function () {
     Route::get('/resources/{type}', 'ResourceController@parse');
 });
